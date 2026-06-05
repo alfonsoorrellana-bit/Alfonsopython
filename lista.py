146168a34acebb82f0113938d0fe401bb9cfda98
nskponerme <alfonso.orrellana@gmail.com>
2026-06-05 11:10:26 -0400

    lista alfonso

---
+++ lista.py
@@ -0,0 +1,65 @@
+# hacer una lista de 3 nombres y otra de 3 apellidos
+# mostrar las listas como si fueran nombre
+# vale decir, diego robles, adolfo hinako, luis mussolini.
+
+# nombres = ["diego", "adolfo", "luis"]
+# apellidos = ["robles", "hinako", "mussolini"]
+
+# for n in range(len(nombres)):
+#     print(nombres[n], apellidos[n])
+
+from unittest import case
+juguetes = ["yo-yo", "pelota", "muñeca"]
+def mostrar():
+     c=1
+     for j in juguetes:
+          print(c,".-",j)  
+          c+=1
+     print("-"*30)
+def actualizar ():
+        mostrar()
+        print("que juguete desea actualizar?")
+        actualizar=int(input())
+        nuevojuguete=input("ingrese el nuevo juguete: ")
+        juguetes[actualizar-1]=nuevojuguete
+def eliminar():
+    mostrar()
+    eliminar=int(input("que juguete desiaria eliminar"))
+    juguetes.pop(eliminar-1)
+    print("juguete eliminado")
+
+while True:
+    try:
+        print("1.- agregar juguete")
+        print("2.- eliminar juguete")
+        print("3.- actualizar juguete")
+        print("4.- mostrar juguete")
+        print("5.- salir")
+        op=int(input("ingrese una opcion: "))
+        match op:
+           case 1:
+                ju=input("agregue un juguete: ")
+                juguetes.append(ju)
+           case 2:
+                mostrar()
+           case 3:
+                actualizar()
+           case 4:
+                mostrar()
+           case 5:
+                print("saliendo")
+                break
+           case _:
+                print("opcion no valida")
+
+                    
+
+    except Exception as e:
+        print("error: ",e)
+    
+    
+          
+
+    
+
+
--- solucion.py
+++ solucion.py
@@ -19,27 +19,27 @@
  
 
 
-while True:
-    try:
-        cantP = int(input("cuantos bultos son"))
-        break
-    except ValueError as e:
-        print("solo valores enteros. error:", e)
-bilvianos = 0
-bnormales = 0
-for i in range(cantP):
-    while True:
-        try:
-            bulto = float(input(f"ingrese el bulto {i+1}: "))
-            if bulto <=5:
-                bilvianos+=1
-            else:
-                bnoreales+=1
-            break
-        except ValueError as e:
-            print("solo valores enteros. error:", e)
+# while True:
+#     try:
+#         cantP = int(input("cuantos bultos son"))
+#         break
+#     except ValueError as e:
+#         print("solo valores enteros. error:", e)
+# bilvianos = 0
+# bnormales = 0
+# for i in range(cantP):
+#     while True:
+#         try:
+#             bulto = float(input(f"ingrese el bulto {i+1}: "))
+#             if bulto <=5:
+#                 bilvianos+=1
+#             else:
+#                 bnoreales+=1
+#             break
+#         except ValueError as e:
+#             print("solo valores enteros. error:", e)
 
 
 
-print(f"bultos livianos: {bilvianos*1000}")
-print(f"bultos normales: {bnormales*2000}")
+# print(f"bultos livianos: {bilvianos*1000}")
+# print(f"bultos normales: {bnormales*2000}")
