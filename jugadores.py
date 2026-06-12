e86184736ccd429e9d2b7283b260f0514a3b8a94
nskponerme <alfonso.orrellana@gmail.com>
2026-06-12 11:59:33 -0400

    12-06-26

--- jugadores.py
+++ jugadores.py
@@ -1,8 +1,108 @@
-{jugadores=
-   1:{"nombre": "Maracuyá", "precio": 3000},
-   2:{"nombre": "Pera", "precio": 1500},
-   3:{"nombre": "Cebolla", "precio": 1200}
+jugadores={
+   1:{"nombre": "cristano ronaldo ", "nacido": 1985, "pais": "Portugal"},
+   2:{"nombre": "lionel andres messi", "nacido": 1987, "pais": "Argentina"},
+   3:{"nombre": "neymar junior", "nacido": 1992, "pais": "Brasil"}
 }
+
+carrito=[]
+def jugadores():
+   print("Cual es el nombre del jugador?")
+   nombre = input()
+   print("cual es el nacimiento?")
+   nacido = int(input())
+   nuevoKey=list(jugadores.keys())
+   nuevoKey.sort()
+   jugadores[nuevoKey[-1]+1]= {"nombre": nombre, "nacido": nacido}
+def Mostrarjugadores():
+   for key, producto in jugadores.items():
+      print(f"{key} .{producto}")
+def eliminarProducto():
+   Mostrarjugadores()
+   borrar=int(input("Cual  Producto borrará?: "))
+   del jugadores[borrar]
+def actualizarProducto():
+   Mostrarjugadores()
+   num=int(input("Que jugador desea actualizar?: "))
+   if num in jugadores.keys():
+      nombre=input("Cual es el nombre nuevo?: ")
+      precio=int(input("Cual es el precio nuevo?: "))
+      productosDicc[num]={"nombre": nombre, "precio": precio}
+   else:
+      print("Producto no encontrado")
+
+def comprar():
+   while True:
+      Mostrarjugadores()
+      try:
+         con=int (input("que jugador desea comprar?: "))
+         if con==0:
+            break
+         if con in jugadores.keys():
+            carrito.append(jugadores[con])
+      except Exception as e:
+           print("Error:",e)
+
+def crearboleta():
+   total=0
+   print("-"*30 "0", "-"*30)
+   print("bienvenido a los nacimientos de los jugadores ")
+   for prod in carrito:
+      print(f"{prod['nombre']}__${prod['precio']}")
+      total+=prod["precio"]
+   print("-"*30 "0", "-"*30)
+   print("gracias por venir a los nacimientos de los jugadores ")
+   print("-"*30 "0", "-"*30)
+         
+# print(productosDicc[2]["precio"])  # precio de la pera
+# print(productosDicc[3]["nombre"])  # nombre de la cebolla
+
+# for num, veg in productosDicc.items():
+#     print(f"{num}.- {veg}")
+
+##Lista con diccionarios
+productosList=[
+   {"nombre": "Maracuyá", "precio": 3000}, #0
+   {"nombre": "Pera", "precio": 1500},     #1  
+   {"nombre": "Cebolla", "precio": 1200}   #2
+]
+
+print(productosList[2]["precio"]) #precio de la cebolla
+print(productosList[0]["nombre"]) #nombre de la naracuya
+
+
+carrito={}
+def vegetalesMenuDiccionario():
+   while True:
+      try:
+         print("-"*20)
+         print("1.- Agregar Vegetal")
+         print("2.- Eliminar Vegetal")
+         print("3.- Actualizar Vegetal")
+         print("4.- Mostrar Vegetal")
+         print("5.- Comprar")
+         print("6.- Crear Boleta y salir")
+         op=int(input("Seleccione una opcion: "))
+         match op:
+               case 1:
+                  agregarProducto()
+               case 2:
+                  eliminarProducto()
+               case 3:
+                  actualizarProducto()
+               case 4:
+                  MostrarProducto()
+               case 5:
+                    print("comprar")
+                    comprar()
+                    carritototal=0
+               case 6:
+                  crearboleta()
+                  break
+               case _:
+                    print("Opcion invalida")  
+      except Exception as e:                 
+         print("Error:",e)
+vegetalesMenuDiccionario()
 
 
 
